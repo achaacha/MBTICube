d654995a031f206d351ac2bb0011157ac196d1d0
Acha <43567591+achaacha@users.noreply.github.com>
2021-01-08 00:34:42 -0600

    Acha_Personal_Library.js
    Removed jQuery to lighten dependency, added personal library. Updated index.html

--- js/cube.js
+++ js/cube.js
@@ -1,15 +1,15 @@
-// START OF UNSURE PART
-
-$('document').ready(function() {
+ready(function(){
   var lastScrollTop = 0;
+  
   //Set the initial state of window.p0 so that scrolling works without clicking
   window.p0 = {
     'x': 0,
     'y': 0
   };
-  $(window).scroll(function trucenscroll(event) {
-    var st = $(this).scrollTop();
-    var sl = $(this).scrollLeft();
+
+  window.scroll(function trucenscroll(event) {
+    var st = this.scrollTop();
+    var sl = this.scrollLeft();
     if (st > lastScrollTop) {
 
       //Le cube tourne
@@ -164,121 +164,3 @@ function init() {
 
   initializeCube();
 }
-
-
-
-// Possibly get Mouse Position to determine rotation?
-//Ref: http://www.quirksmode.org/js/events_properties.html#position
-
-/*const getMousePos = (e) => {
-  let posX = 0;
-  let posY = 0;
-
-  if (!e) var e = window.event;
-  if (e.pageX || e.pageY) {
-    posX = e.pageX;
-    posY = e.pageY;
-  } else if (e.clientX || e.clientY) {
-    posX = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-    posY = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-  }
-
-  const cube = document.querySelector('.cube');
-
-  //Splits mousePos based on xy axis: -50 -> 50
-  const pX = (posX / window.innerWidth) * 100 - 50;
-  const pY1 = (posY / window.innerHeight) * 100 - 50;
-
-  const pY = pY1 * -1;
-
-  If 1% = 3.6 degrees and 1% of 50 = .5,
-  then 3.6/.5 = 7.2.
-  7.2 * pX (or pY) = correct degree.
-  If Rotx = 256.29, Posx= 832, Px= 35....
-
-
-  Cube is rotating when loading into the session
-  When mouse is clicked
-  Cube reposition itself to 0degs on both x and y axis
-  Have Sessionstorage record mouse drag based on an If Else statement
-
-  IE: If mouse goes +(2,2) points from original point on the grid
-  (where the mouse down position first started at),
-  then rotate cube x degrees based on direction and degree calculation.
-  Else if Cube goes -(-2,-2) points from original point,
-  rotate cube in opposite direction based on direction and degree calculation.
-  Else if Cube goes (+2.-2)...... so on so forth
-
-  Have session data store information in CSS transformation properties
-  One to record new degree thats applied to the transformation*/
-
-  /*let rotX = pX * 7.2;
-  let rotY = pY * 7.2;
-
-  /*console.log("Rot x:" + rotX + ", y:" + rotY);
-  console.log("Pos x:" + posX + ", y:" + posY);
-  console.log("P x:" + pX + ", y:" + pY);
-
-  var rX = sessionStorage.getItem('recordX');
-  var rY = sessionStorage.getItem('recordY');
-
-  const saveDegreeDrag = () => {
-    sessionStorage.setItem('recordX', rotX);
-    sessionStorage.setItem('recordY', rotY);
-  }
-
-  const rotateCube = () => {
-
-    /*if (rotX > 0 && rotY > 0) {
-      rotX = 0;
-      rotY = 0;
-
-      let mathX = rX + rotX;
-      let mathY = rY + rotY;
-
-      let newRot = `rotateY(${mathX}deg) rotateX(${mathY}deg)`;
-      let difX = rX - rotX
-
-      if (pX > 0 && pY > 0) {
-
-        let newRot = `rotateY(${rotX}deg) rotateX(${rotY}deg)`;
-        cube.style.transform = newRot;
-      }
-
-      /*console.log("Session Stored X:" + rX + ", And Y:" + rY);
-      console.log("Difference for X only:" + difX);
-      saveDegreeDrag();
-    //}
-  }
-
-
-  rotateCube();
-
-
-  window.addEventListener('mouseup', e => {
-    isDragging = false;
-
-    if (isDragging === false) {
-      window.removeEventListener('mousemove', getMousePos);
-    }
-
-  });
-} //End getMousePos
-
-let isDragging = false;
-
-window.addEventListener('mousedown', e => {
-  isDragging = true;
-
-  if (isDragging === true) {
-    window.addEventListener('mousemove', getMousePos);
-  }
-});
-
-window.addEventListener('mousemove', e => {
-
-  if (isDragging === true) {
-    window.addEventListener('mousemove', getMousePos);
-  }
-  isDragging = false;
-});*/
--- js/translations.js
+++ js/translations.js
@@ -1,3 +1,18 @@
+//Difference between '$(this)'' and 'this':
+//https://stackoverflow.com/questions/1051782/whats-the-difference-between-this-and-this
+
+//https://stackoverflow.com/questions/799981/document-ready-equivalent-without-jquery
+function ready(callback){
+    // in case the document is already rendered
+    if (document.readyState!='loading') callback();
+    // modern browsers
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    // IE <= 8
+    else document.attachEvent('onreadystatechange', function(){
+        if (document.readyState=='complete') callback();
+    });
+}
+
 /** A storage solution aimed at replacing jQuerys data function.
  * Implementation Note: Elements are stored in a (WeakMap)[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakMap].
  * This makes sure the data is garbage collected when the node is removed.
@@ -24,7 +39,20 @@ window.dataStorage = {
         return ret;
     }
 }
-
 /*Use it like this:
 var myElement = document.getElementById("myId");
 dataStorage.put(myElement, "myKey", "myValue"); */
+
+
+/*-------------------------------------------------------------
+|                    THE MOST FKIN BEAUTIFUL                  |
+|                    THING I HAVE EVER SEEN                   |
+|                         IN MY LIFE                          |
+|                    THANK YOU DANC-SENPAI                    |
+--------------------------------------------------------------*/
+
+var $ = document.querySelector.bind(document);
+var $$ = document.querySelectorAll.bind(document);
+
+// use as= $('#something').whatever();
+// or  var item = $$('#something .item');
